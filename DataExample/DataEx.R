6b1a6c93c06569129289442ce973d1cd2f210e30
Bryan <bstanfill2003@gmail.com>
2014-03-02 21:04:13 +1000

    update data example code

--- DataExample/DataEx.R
+++ DataExample/DataEx.R
@@ -1,23 +1,50 @@
+source("../intervalsSO3/intervals/MedianPaper/PrepareData.R")
+sourceCpp('Source_Code/MeanMedianAsy.cpp')
 
 #####################
 #### Find all locations where mean/median differ substantially
 #####################
 
-#Randomly select a location with enough spread to be interesting
 possibles<-which(loc.stats$dE>.1 & loc.stats$dE<.15)
 possibles<-sort(c(possibles,which.max(loc.stats$dE)))
 m<-length(possibles)
-DataExDF<-data.frame(Location=rep(0,m),MeanNTH=rep(0,m),MedianNTH=rep(0,m),MeanBoot=rep(0,m),MedianBoot=rep(0,m))
+DataExDF<-data.frame(Location=rep(0,m),MeanE=0,MedianE=0,GMedianE=0,WMeanE=0,MeanR=0,MedianR=0,GMedianR=0,WMeanR=0)
 
 for(i in 1:m){
   ex<-loc.stats[possibles[i],]$location 
   DataExDF$Location[i]<-ex
   exRots<-as.SO3(data.matrix(dat.out[dat.out$location==ex,3:11]))
-  DataExDF$MeanNTH[i]<-region(exRots,method='moment',type='theory',estimator='mean',alp=.1)*180/pi
-  DataExDF$MedianNTH[i]<-region(exRots,method='moment',type='theory',estimator='median',alp=.1)*180/pi
-  DataExDF$MeanBoot[i]<-region(exRots,method='moment',type='bootstrap',estimator='mean',alp=.1,m=500)*180/pi
-  DataExDF$MedianBoot[i]<-region(exRots,method='moment',type='bootstrap',estimator='median',alp=.1,m=500)*180/pi
+  Qs<-as.Q4(exRots)
+  
+  L2<-mean(Qs)
+  DataExDF$MeanE[i]<-sum(rot.dist(Qs,L2))
+  DataExDF$MeanR[i]<-sum(rot.dist(Qs,L2,method='intrinsic'))
+  
+  L1<-median(Qs)
+  DataExDF$MedianE[i]<-sum(rot.dist(Qs,L1))
+  DataExDF$MedianR[i]<-sum(rot.dist(Qs,L1,method='intrinsic'))
+  
+  G1<-median(Qs,type='geometric')
+  DataExDF$GMedianE[i]<-sum(rot.dist(Qs,G1))
+  DataExDF$GMedianR[i]<-sum(rot.dist(Qs,G1,method='intrinsic'))
+  
+  Hn<-as.vector(HnCpp(Qs))
+  WL1<-weighted.mean(Qs,w=1/Hn)
+  DataExDF$WMeanE[i]<-sum(rot.dist(Qs,WL1))
+  DataExDF$WMeanR[i]<-sum(rot.dist(Qs,WL1,method='intrinsic'))
 }
 
-DataExDF$Location<-as.factor(DataExDF$Location)
-xtable(DataExDF,digits=3)
+
+# DataExDF<-data.frame(Location=rep(0,m),MeanNTH=rep(0,m),MedianNTH=rep(0,m),MeanBoot=rep(0,m),MedianBoot=rep(0,m))
+# for(i in 1:m){
+#   ex<-loc.stats[possibles[i],]$location 
+#   DataExDF$Location[i]<-ex
+#   exRots<-as.SO3(data.matrix(dat.out[dat.out$location==ex,3:11]))
+#   DataExDF$MeanNTH[i]<-region(exRots,method='moment',type='theory',estimator='mean',alp=.1)*180/pi
+#   DataExDF$MedianNTH[i]<-region(exRots,method='moment',type='theory',estimator='median',alp=.1)*180/pi
+#   DataExDF$MeanBoot[i]<-region(exRots,method='moment',type='bootstrap',estimator='mean',alp=.1,m=500)*180/pi
+#   DataExDF$MedianBoot[i]<-region(exRots,method='moment',type='bootstrap',estimator='median',alp=.1,m=500)*180/pi
+# }
+# 
+# DataExDF$Location<-as.factor(DataExDF$Location)
+# xtable(DataExDF,digits=3)
